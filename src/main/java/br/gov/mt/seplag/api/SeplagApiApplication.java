4c7b10c75334575880e2e6acf3f886a6922addad
Arão Farias <arao.alves7@gmail.com>
2025-03-27 10:07:19 -0300

    Iniciando aplicação

---
+++ src/main/java/br/gov/mt/seplag/api/SeplagApiApplication.java
@@ -0,0 +1,13 @@
+package br.gov.mt.seplag.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeplagApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeplagApiApplication.class, args);
+	}
+
+}
